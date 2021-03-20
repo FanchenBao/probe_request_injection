0fab92f2ee6c7b4312aed2a63a962e796138a6bf
FanchenBao <jonathonbao@gmail.com>
2021-03-19 20:01:49 -0400

    Add command line argument parser to emit.sh

--- emit/emit.sh
+++ emit/emit.sh
@@ -1,12 +1,63 @@
 #!/bin/bash
 
-# Set wlan1 to monitor mode and emitting on channel 10
-sudo ifconfig wlan1 down && sudo iwconfig wlan1 mode monitor && sudo ifconfig wlan1 up
-sudo iwconfig wlan1 channel 10
+main() {
+  if [[ $INTERFACE == "" || $CHANNEL == "" ]]; then
+      print_usage
+      exit 1
+  fi
+
+  # Set target interface to monitor mode and emitting on channel 10
+  sudo ifconfig $INTERFACE down && sudo iwconfig $INTERFACE mode monitor && sudo ifconfig $INTERFACE up
+  sudo iwconfig $INTERFACE channel $CHANNEL
+
+  # Perform probe request injection in a tmux pane.
+  tmux new -s emit -d
+  tmux send-keys -t emit 'cd $HOME/probe_request_injection' Enter
+  tmux send-keys -t emit 'sudo su' Enter
+  tmux send-keys -t emit 'source venv/bin/activate' Enter
+  tmux send-keys -t emit 'python3 emit/emit_probe_request.py' Enter
+}
 
 
-tmux new -s emit -d
-tmux send-keys -t emit 'cd $HOME/probe_request_injection' Enter
-tmux send-keys -t emit 'sudo su' Enter
-tmux send-keys -t emit 'source venv/bin/activate' Enter
-tmux send-keys -t emit 'python3 emit/emit_probe_request.py' Enter
+# DEFAULTS
+INTERFACE=""
+CHANNEL=""
+
+print_usage() {
+  printf "%s\n" "Usage: emit.sh [-i wifi_interface] [-c channel]"
+  printf "%s\t%s\n" "-i" "WiFi device interface in monitor mode. Type \"iwconfig\" to locate the target interface. Required." \
+    "-c" "The channel on which probe request is injected. Required."
+}
+
+# Parse options and flags
+while [[ "$#" -gt 0 ]]; do
+  case "$1" in
+    -i)  # wifi device interface that is in monitor mode
+      if [[ "$2" != "" ]]; then
+        INTERFACE="$2"
+        shift 1
+      else # -i must be followed by a second argument
+        print_usage
+        exit 1
+      fi
+      shift 1
+      ;;
+    -c)  # Channel
+      if [[ "$2" != "" ]]; then
+        CHANNEL="$2"
+        shift 1
+      else # -i must be followed by a second argument
+        print_usage
+        exit 1
+      fi
+      shift 1
+      ;;
+    *) # unsupported flags
+      print_usage
+      exit 1
+      ;;
+  esac
+done
+
+
+main
